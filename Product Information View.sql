43ead5d6788f8f34662eb9efaf37373e80bb91d0
jared-mindel <153224910+jared-mindel@users.noreply.github.com>
2023-12-07 12:28:10 -0600

    Add files via upload
    First upload; including the definition of the SQL view as well as the pbix file.

---
+++ Product Information View.sql
@@ -0,0 +1,40 @@
+USE AdventureWorks2019
+GO
+
+CREATE OR ALTER VIEW "Product Information" AS (
+	SELECT DISTINCT
+		p.Name AS "Product",
+		p.StandardCost AS "Cost",
+		p.ListPrice AS "Price",
+		p.Color,
+		p.Size,
+		p.Weight,
+		p.Class,
+		p.Style,
+		p.DiscontinuedDate,
+		pc.Name AS "Product Category",
+		psc.Name AS "Product Subcategory",
+		pr.Rating,
+		pr.Comments,
+		l.Name AS "Location",
+		CASE 
+			WHEN 
+				p.ListPrice < 1000
+				THEN 'Cheaper'
+			WHEN 
+				p.ListPrice BETWEEN 1000 AND 1999
+				THEN 'Expensive'
+			WHEN 
+				p.ListPrice > 2000
+				THEN 'Very Expensive'
+		END AS "Price Category"
+
+	FROM 
+		Production.Product p 
+		LEFT JOIN Production.ProductSubcategory psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+		LEFT JOIN Production.ProductCategory pc ON pc.ProductCategoryID = psc.ProductCategoryID
+		LEFT JOIN Production.ProductReview pr ON p.ProductID = pr.ProductID
+		LEFT JOIN Production.ProductInventory piv ON p.ProductID = p.ProductID
+		JOIN Production.Location l ON piv.LocationID = l.LocationID
+
+)
